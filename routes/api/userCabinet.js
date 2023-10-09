8ead20ca1f22d525f9b566ce334d777e1c301ae9
Nick Leso <nickleso.work@gmail.com>
2023-10-09 14:18:41 +0300

    Update userCabinet.js

--- routes/api/userCabinet.js
+++ routes/api/userCabinet.js
@@ -2,12 +2,16 @@ const express = require("express");
 const userCabinet = express.Router();
 
 const { ctrlWrapper, getUserData, upload } = require("../../middlewares");
-const { changeVeteranStatus } = require("../../controllers/userCabinet");
+const {
+  changeVeteranStatus,
+} = require("../../controllers/userCabinet/changeVeteranStatus");
+
+changeVeteranStatus;
 
 // change vetaran stutus and send email
 userCabinet.post(
   "/vateran/change-status",
-  upload,
+  upload.array("files"),
   getUserData,
   ctrlWrapper(changeVeteranStatus)
 );
